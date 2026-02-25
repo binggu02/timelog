8d8f803cf135f7cbf8cfee7a41b64a4c3348e8e4
BH <kbh02180@gamil.com>
2026-02-26 02:13:13 +0900

    timelog 메이븐 프로젝트 생성

---
+++ src/main/java/com/toyproject/timelog/TimelogApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.timelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyproject/timelog/TimelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.timelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
